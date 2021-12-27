a806140a6f1768e319c082521c398da9119ed79f
d-pettersson <david.pettersson@code.berlin>
2021-12-27 17:24:04 +0100

    Readd console output

--- post-install.sh
+++ post-install.sh
@@ -3,53 +3,53 @@ echo "Welcome to Binia's Centos 7 post install script! "
 
 echo "Fist will update system... "
 
-yum update -yyy &>/dev/null
+yum update -yyy 
 
 echo "System is updated"
 
 echo "Adding Epel repo... "
 
-yum install epel-release -y &>/dev/null
+yum install epel-release -y
 
 echo "Done! "
 
-yum update &>/dev/null
+yum update
 
 echo "Installing Development tools and few other things... "
 
-yum install nano git -y &>/dev/null
-yum group install "Development Tools" -y &>/dev/null
-yum install yum-utils device-mapper-persistent-data lvm2 -y &>/dev/null
-yum install python-pip -y &>/dev/null
-pip install --upgrade pip &>/dev/null
+yum install nano git -y
+yum group install "Development Tools" -y
+yum install yum-utils device-mapper-persistent-data lvm2 -y
+yum install python-pip -y
+pip install --upgrade pip
 
 echo "Done! "
 
 echo "Adding Docker Community Edition repository and installing docker-ce with docker-compose... "
-curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose &>/dev/null
-cp /usr/local/bin/docker-compose /usr/bin/docker-compose &>/dev/null
-chmod +x /usr/bin/docker-compose &>/dev/null
-yum upgrade python* &>/dev/null
-yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo &>/dev/null
-yum update &>/dev/null
-yum install docker-ce -yy &>/dev/null
-systemctl start docker &>/dev/null
-systemctl enable docker &>/dev/null
+curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+cp /usr/local/bin/docker-compose /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+yum upgrade python*
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum update
+yum install docker-ce -yy
+systemctl start docker
+systemctl enable docker
 
 echo "Done! "
 
 echo "Installing NTP syncing..."
-yum install ntp ntpdate &>/dev/null
-systemctl start ntpd &>/dev/null
-systemctl enable ntpd &>/dev/null
-ntpdate -u -s 0.uk.pool.ntp.org 1.uk.pool.ntp.org 2.uk.pool.ntp.org 3.uk.pool.ntp.org &>/dev/null
-systemctl restart ntpd &>/dev/null
-hwclock -w &>/dev/null
+yum install ntp ntpdate
+systemctl start ntpd
+systemctl enable ntpd
+ntpdate -u -s 0.uk.pool.ntp.org 1.uk.pool.ntp.org 2.uk.pool.ntp.org 3.uk.pool.ntp.org
+systemctl restart ntpd
+hwclock -w
 
 echo "Done!"
 
-mkdir .ssh &>/dev/null
-touch .ssh/authorized_keys &>/dev/null
+mkdir .ssh
+touch .ssh/authorized_keys
 chmod -R 600 .ssh/
 
 ## Azuracast docker install ##
